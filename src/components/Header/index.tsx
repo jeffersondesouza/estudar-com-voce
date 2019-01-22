e3586be7502455bc2d37cd7bf7d9a437900f7113
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-01-22 17:44:22 -0300

    sty: seacr on desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export interface IProps {
 
 export const Header = (props: IProps) => (
   <header className="header">
-    <figure className="header__logo-fig">
+    <figure className="header__brand">
       <img className="header__logo-img" src=" src /assets/img/logo.png" alt="Logotipo Estudar Com Você " srcSet="src/assets/img/logo.png" />
     </figure>
     <div className="search">
