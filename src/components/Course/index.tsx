89c1eeb3e883c9b86f98d34fd6f3b57515158259
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-01-23 14:31:09 -0300

    feat: links pn header and courses

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/styles.scss';
 import { Course } from '../../models/Course';
@@ -12,13 +13,13 @@ export const CourseComponent = (props: IProps) => {
   const { course } = props;
   const classNameHeader = course.generic ? 'course__header' : 'course__header course__header--generic';
   return (
-    <div className="course">
+    <Link to={`courses/${course.id}`} className="course">
       <div className={classNameHeader}>
         <h3>{course.name}</h3>
       </div>
       <div className="course__footer">
         <div className="course__highlight">{course.highlight && 'Novo!'}</div>
       </div>
-    </div>
+    </Link>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import './styles/styles.scss';
+import { Link } from 'react-router-dom';
 
 
 export interface IProps {
@@ -9,8 +10,10 @@ export interface IProps {
 
 export const Header = (props: IProps) => (
   <header className="header">
-    <figure className="header__brand">
-      <img className="header__logo-img" src=" src /assets/img/logo.png" alt="Logotipo Estudar Com Você " srcSet="src/assets/img/logo.png" />
-    </figure>
+    <Link to="/">
+      <figure className="header__brand">
+        <img className="header__logo-img" src=" src /assets/img/logo.png" alt="Logotipo Estudar Com Você " srcSet="src/assets/img/logo.png" />
+      </figure>
+    </Link>
   </header>
 );
